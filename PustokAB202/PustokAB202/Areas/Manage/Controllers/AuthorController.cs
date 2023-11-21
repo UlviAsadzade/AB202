1a9632e89def3fb91dcda008c1e2f96d833e08f0
ulvi1993 <ulvi.esedzadeh@gmail.com>
2023-11-21 19:20:35 +0400

    CRUD intro

---
+++ PustokAB202/PustokAB202/Areas/Manage/Controllers/AuthorController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PustokAB202.DAL;
+using PustokAB202.Models;
+
+namespace PustokAB202.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class AuthorController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AuthorController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Author> authors = await _context.Authors.Include(x=>x.Books).ToListAsync();
+            return View(authors);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Author author)
+        {
+            bool result =await _context.Authors.AnyAsync(x=>x.Fullname== author.Fullname);
+
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (result)
+            {
+                ModelState.AddModelError("Fullname", "Eyni adli yazici yarana bilmez");
+                return View();
+            }
+
+            await _context.AddAsync(author);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- PustokAB202/PustokAB202/Models/Author.cs
+++ PustokAB202/PustokAB202/Models/Author.cs
@@ -1,11 +1,15 @@
-﻿namespace PustokAB202.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PustokAB202.Models
 {
 	public class Author
 	{
 		public int Id { get; set; }
 
+		[Required(ErrorMessage ="Ad mutleqdir")]
+
 		public string Fullname { get; set; }
 
-		public List<Book> Books { get; set; }
+		public List<Book>? Books { get; set; }
 	}
 }
