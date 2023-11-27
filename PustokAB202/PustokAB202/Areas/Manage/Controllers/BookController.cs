40090baa86d9b706b4f8204b30042d4e2f6cb7a8
ulvi1993 <ulvi.esedzadeh@gmail.com>
2023-11-27 19:18:22 +0400

    one to many update

--- PustokAB202/PustokAB202/Areas/Manage/Controllers/BookController.cs
+++ PustokAB202/PustokAB202/Areas/Manage/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PustokAB202.Areas.Manage.ViewModels;
+using PustokAB202.Areas.Manage.ViewModels;
 using PustokAB202.DAL;
 using PustokAB202.Models;
 
@@ -107,6 +108,78 @@ namespace PustokAB202.Areas.Manage.Controllers
         }
         #endregion
 
+        public async Task<IActionResult> Update(int id)
+        {
+            Book book = await _context.Books.FirstOrDefaultAsync(x=>x.Id == id);
+
+            if (book == null) return NotFound();
+
+            BookUpdateVM updateVM = new BookUpdateVM
+            {
+                Name = book.Name,
+                AuthorId = book.AuthorId,
+                GenreId = book.GenreId,
+                Page = book.Page,
+                SalePrice = book.SalePrice,
+                CostPrice = book.CostPrice,
+                Discount = book.Discount,
+                Desc = book.Desc,
+                IsAvailable = book.IsAvailable,
+                Authors = await _context.Authors.ToListAsync(),
+                Genres = await _context.Genres.ToListAsync(),
+
+            };
+
+            return View(updateVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, BookUpdateVM updateVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                updateVM.Authors = await _context.Authors.ToListAsync();
+                updateVM.Genres = await _context.Genres.ToListAsync();
+                return View(updateVM);
+            }
+
+            Book exist = await _context.Books.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (exist == null) return NotFound();
+
+
+            if (await _context.Authors.FirstOrDefaultAsync(x => x.Id == updateVM.AuthorId) is null)
+            {
+                ModelState.AddModelError("AuthorId", "This Author is not exist");
+                updateVM.Authors = await _context.Authors.ToListAsync();
+                updateVM.Genres = await _context.Genres.ToListAsync();
+                return View(updateVM);
+            }
+
+            if (await _context.Genres.FirstOrDefaultAsync(x => x.Id == updateVM.GenreId) is null)
+            {
+                ModelState.AddModelError("GenreId", "This Genre is not exist");
+                updateVM.Authors = await _context.Authors.ToListAsync();
+                updateVM.Genres = await _context.Genres.ToListAsync();
+                return View(updateVM);
+            }
+
+            exist.Name = updateVM.Name;
+            exist.SalePrice = updateVM.SalePrice;
+            exist.CostPrice = updateVM.CostPrice;
+            exist.Discount= updateVM.Discount;
+            exist.AuthorId = updateVM.AuthorId;
+            exist.GenreId= updateVM.GenreId;
+            exist.Page= updateVM.Page;
+            exist.Desc= updateVM.Desc;
+            exist.IsAvailable= updateVM.IsAvailable;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
         #region Delete
         public async Task<IActionResult> Delete(int id)
         {
@@ -117,9 +190,10 @@ namespace PustokAB202.Areas.Manage.Controllers
             book.IsDeleted = true;
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        } 
+        }
         #endregion
 
+        #region Detail
         public async Task<IActionResult> Detail(int id)
         {
             Book book = await _context.Books
@@ -131,6 +205,7 @@ namespace PustokAB202.Areas.Manage.Controllers
             if (book == null) return NotFound();
 
             return View(book);
-        }
+        } 
+        #endregion
     }
 }
